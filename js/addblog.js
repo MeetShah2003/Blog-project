707084eeed03cee4a8ebbf3c67d71581e7fbd181
MeetShah2003 <meetshah21102003@gmail.com>
2023-09-07 15:39:56 +0530

    UI Done

--- js/addblog.js
+++ js/addblog.js
@@ -1,23 +1,23 @@
 import { navbar } from "../components/navbar.js";
 
-document.getElementById("navbar").innerHTML=navbar();
+document.getElementById("navbar").innerHTML = navbar();
 
 
-const blogPost=((e)=>{
+const blogData=(e)=>{
     e.preventDefault();
-    let blogData={
+    let blog={
         title:document.getElementById("title").value,
         category:document.getElementById("category").value,
         img:document.getElementById("img").value,
-        description:document.getElementById("description").value
+        description:document.createElement("description").value
     };
 
     fetch(`http://localhost:1020/blog`,{
         method:"POST",
         headers:{"Content-Type":"application/json"},
-        body:JSON.stringify(blogData)
+        body:JSON.stringify(blog)
     });
-});
+};
 
 
-document.getElementById("form").addEventListener("submit",blogPost);
+document.getElementById("form").addEventListener("submit",blogData);
--- js/blog.js
+++ js/blog.js
@@ -1,3 +1,36 @@
 import { navbar } from "../components/navbar.js";
 
 document.getElementById("navbar").innerHTML=navbar();
+
+let blog=[];
+
+const uimaker=(data)=>{
+    document.getElementById("ui").innerHTML="";
+    data.map((item)=>{
+        let img=document.createElement("img");
+        img.src=item.img;
+        let title=document.createElement("h2");
+        title.innerHTML=item.title;
+        let category=document.createElement("h3");
+        category.innerHTML=item.category;
+        let description=document.createElement("p");
+        description.innerHTML=item.description;
+        let btn1=document.createElement("button");
+        btn1.innerHTML="Like"
+        let btn2=document.createElement("button");
+        btn2.innerHTML="Delete";
+        let div=document.createElement("div");
+        div.append(img,title,category,btn1,btn2);
+        document.getElementById("ui").append(div);
+    });
+}
+
+const getData=async()=>{
+    fetch(`http://localhost:1020/blog`)
+    .then((res)=>res.json())
+    .then((data)=>{
+        blog=data;
+        uimaker(data);
+    })
+};
+getData();
